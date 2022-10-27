500d98f3ff049f9b4662ccadc2ffe561c494bb28
Zahed75 <zahedhasan.cs@gmail.com>
2022-10-27 18:24:46 +0600

    USERS API DONE

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,18 @@
-const express=require('express')
+const express = require('express')
+const AuthVerifyMiddleware = require("../middlewares/AuthVerifyMiddleware");
+const UserController = require("../controllers/Users/UsersControllers")
+const router = express.Router();
 
 
-const router=express.Router();
+//UserProfile
+
+router.post("/Registration", UserController.Registration);
+router.post("/Login", UserController.Login);
+router.post("/ProfileUpdate", AuthVerifyMiddleware, UserController.ProfileUpdate);
+router.get("/ProfileDetails", AuthVerifyMiddleware, UserController.ProfileDetails);
+router.get("/RecoverVerifyEmail/:email", UserController.RecoverVerifyEmail);
+router.get("/RecoverVerifyOTP/:email/:otp", UserController.RecoverVerifyOTP);
+router.post("/RecoverResetPass", UserController.RecoverResetPass);
 
 
-module.exports=router;
+module.exports = router;
